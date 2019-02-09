be69ddc137d992c969623d798604072aa61db3df
Grzegorz Abramczyk <abramczykg@gmail.com>
2019-02-09 21:59:13 +0100

    Loading command center config

--- load/index.ts
+++ load/index.ts
@@ -1,6 +1,5 @@
 import * as debug from 'debug'
 import { merge , override} from 'ouch-rx'
-import { tap } from 'rxjs/operators'
 import { db } from './pouchdb'
 import { file } from './read'
 import { yaml } from './yaml'
@@ -8,4 +7,7 @@ const log = debug('load')
 
 db('progress-goal').then((pgdb) => file('data/progress-goal.yaml')
   .pipe(yaml(), merge(pgdb, override))
+  .subscribe(log, log))
+db('command-center-config').then((cccdb) => file('data/command-center-config.yaml')
+  .pipe(yaml(), merge(cccdb, override))
   .subscribe(log, log))
